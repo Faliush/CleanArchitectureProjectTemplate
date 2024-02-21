871700cbd0e2ef6212dc8d6b8c82e16d308ed297
nickfaliush <faliushdev@gmail.com>
2024-02-21 20:32:11 +0200

    Fixed bug with hosted service

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure.Interceptors;
+﻿using Infrastructure.DatabaseInitialization;
+using Infrastructure.Interceptors;
 using Infrastructure.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
@@ -11,6 +12,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddHostedService<MigrationService>();
+
         services.AddSingleton<ISaveChangesInterceptor, AuditableEntityInterceptor>();
 
         services.AddDbContext<ApplicationDbContext>(
