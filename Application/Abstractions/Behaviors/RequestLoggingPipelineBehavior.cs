1632019f9f5cd0186c6de403cb1ffdac8d61233c
nickfaliush <faliushdev@gmail.com>
2024-04-16 16:49:13 +0300

    Fixed error

--- Application/Abstractions/Behaviors/RequestLoggingPipelineBehavior.cs
+++ Application/Abstractions/Behaviors/RequestLoggingPipelineBehavior.cs
@@ -1,13 +1,14 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Logging;
 using Serilog.Context;
+using Domain.Core.Primitives.Result;
 
 namespace Application.Abstractions.Behaviors;
 
 internal sealed class RequestLoggingPipelineBehavior<TRequest, TResponse>
     : IPipelineBehavior<TRequest, TResponse>
     where TRequest : class
-    where TResponse : OperationResult.OperationResult
+    where TResponse : Result
 {
     private readonly ILogger _logger;
 
