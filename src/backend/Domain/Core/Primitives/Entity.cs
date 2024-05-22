a55e5839378c46a77130626171f2f048f8bbb184
nickfaliush <faliushdev@gmail.com>
2024-05-22 14:41:47 +0300

    Slightly improved base entity class

--- src/backend/Domain/Core/Primitives/Entity.cs
+++ src/backend/Domain/Core/Primitives/Entity.cs
@@ -9,45 +9,34 @@ public abstract class Entity : IHaveId, IEquatable<Entity>
 
     protected Entity() {  }
 
-    public Guid Id { get; private set; }
+    public Guid Id { get; private init; }
 
-    public static bool operator ==(Entity a, Entity b)
-    {
-        if (a is null && b is null)
-        {
-            return true;
-        }
+    public static bool operator == (Entity first, Entity second)
+        => first is not null && second is not null && first.Equals(second);
 
-        if (a is null || b is null)
-        {
-            return false;
-        }
+    public static bool operator != (Entity first, Entity second) 
+        => !(first == second);
 
-        return a.Equals(b);
-    }
-
-    public static bool operator !=(Entity a, Entity b) => !(a == b);
-
-    public bool Equals(Entity other)
+    public bool Equals(Entity? other)
     {
         if (other is null)
         {
             return false;
         }
 
-        return ReferenceEquals(this, other) || Id == other.Id;
-    }
-
-    public override bool Equals(object obj)
-    {
-        if (obj is null)
+        if (other.GetType() != GetType())
         {
             return false;
         }
 
-        if (ReferenceEquals(this, obj))
+        return other.Id == Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
         {
-            return true;
+            return false;
         }
 
         if (obj.GetType() != GetType())
@@ -56,11 +45,6 @@ public abstract class Entity : IHaveId, IEquatable<Entity>
         }
 
         if (obj is not Entity other)
-        {
-            return false;
-        }
-
-        if (Id == Guid.Empty || other.Id == Guid.Empty)
         {
             return false;
         }
